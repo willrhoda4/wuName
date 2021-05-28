29e0178f399a6611a076ee9889a2e9aa2c144f91
willrhoda4 <willrhoda4@gmail.com>
2021-05-28 13:48:59 -0500

    Score for first name

--- wuName/wuName.js
+++ wuName/wuName.js
@@ -1,3 +1,20 @@
-const printHello = () => { 
-    console.log('hello'); 
-  }; 
+
+
+const myWuNameIs = (firstName) => {
+  var smallName = firstName.toLowerCase();
+  var nameArr = smallName.split('');
+  var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var nameScore = [];
+  for (let i = 0; i < nameArr.length; i++) {
+    nameScore.push(alphabet.indexOf(nameArr[i]) + 1)
+  }
+
+  var totalScore = nameScore.reduce((a, b) => a + b, 0)
+  return totalScore;
+} ;
+
+console.log(myWuNameIs("Will"));
+
+
+
+/**/
