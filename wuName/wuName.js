5279b3b3491e1172eee033ce6cdd1db62e1246d7
willrhoda4 <willrhoda4@gmail.com>
2021-05-28 14:05:19 -0500

    scores for both names

--- wuName/wuName.js
+++ wuName/wuName.js
@@ -1,19 +1,31 @@
 
 
-const myWuNameIs = (firstName) => {
-  var smallName = firstName.toLowerCase();
-  var nameArr = smallName.split('');
+const myWuNameIs = (firstName, lastName) => {
   var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var nameScore = [];
-  for (let i = 0; i < nameArr.length; i++) {
-    nameScore.push(alphabet.indexOf(nameArr[i]) + 1)
-  }
 
-  var totalScore = nameScore.reduce((a, b) => a + b, 0)
-  return totalScore;
+  var smallFirstName = firstName.toLowerCase();
+  var firstNameArr = smallFirstName.split('');
+  var firstNameScore = [];
+  for (let i = 0; i < firstNameArr.length; i++) {
+    firstNameScore.push(alphabet.indexOf(firstNameArr[i]) + 1)
+  }
+  var firstNameTotal = firstNameScore.reduce((a, b) => a + b, 0)
+
+  var smallLastName = lastName.toLowerCase();
+  var lastNameArr = smallLastName.split('');
+  var lastNameScore = [];
+  for (let i = 0; i < lastNameArr.length; i++) {
+    lastNameScore.push(alphabet.indexOf(lastNameArr[i]) + 1)
+  }
+  var lastNameTotal = lastNameScore.reduce((a, b) => a + b, 0)
+
+
+
+
+  return `first name: ${firstNameTotal} last name: ${lastNameTotal}`;
 } ;
 
-console.log(myWuNameIs("Will"));
+console.log(myWuNameIs("Will", "Rhoda"));
 
 
 
